56342b19b8b9f6cb0b50e9fb70a21ed62669f54f
maximkorikov <96551798+maximkorikov@users.noreply.github.com>
2025-06-15 20:53:30 +0300

    Update 4C_AWG.sh

--- 4C_AWG.sh
+++ 4C_AWG.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Загрузка и запуск скрипта для удаления предыдущей установки (если есть)
+printf "\033[32;1mDownloading and running uninstallation script (if it exists)...\033[0m\n"
+wget -q -O /tmp/uninstall_podkop_awg.sh https://raw.githubusercontent.com/maximkorikov/Router/refs/heads/main/uninstall_podkop_awg.sh && chmod +x /tmp/uninstall_podkop_awg.sh && sh /tmp/uninstall_podkop_awg.sh || echo "Uninstallation script not found or failed."
+
 # 1. Установка Podkop v0.2.5
 printf "\033[32;1mInstalling Podkop v0.2.5...\033[0m\n"
 (echo "3"; echo "n"; echo "y") | sh <(wget -O - https://raw.githubusercontent.com/itdoginfo/podkop/a6a171ef47d0ea91d046a9d613570b2a7c952b0d/install.sh | sed 's|https://api.github.com/repos/itdoginfo/podkop/releases/latest|https://api.github.com/repos/itdoginfo/podkop/releases/tags/v0.2.5|g')
